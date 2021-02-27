1e84bfb30b2d74d0622d3c040b610e314011bb94
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-02-27 17:05:28 -0500

    Update new_entry_screen.dart

--- lib/screens/new_entry_screen.dart
+++ lib/screens/new_entry_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:journal/screens/journal_entries_screen.dart';
+import '../widgets/journal_entry_form.dart';
 
 class NewEntry extends StatelessWidget {
   
@@ -11,12 +12,7 @@ class NewEntry extends StatelessWidget {
       appBar: AppBar(
         title: const Text('New Entry'),
       ),
-      body: Center(child: Text('This is the new entry page.')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.of(context).pushNamed(JournalEntries.routeName),
-        tooltip: 'Add a journal entry.',
-        child: const Icon(Icons.add),
-      ),
+      body: JournalEntryForm()
     );
   }
 }
