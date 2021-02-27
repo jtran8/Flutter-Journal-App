bcd779bf3892c35fec5a69c665857f1265339206
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-02-26 23:19:39 -0500

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'screens/entry_details_screen.dart';
+import 'screens/journal_entries_screen.dart';
+import 'screens/new_entry_screen.dart';
 
 class App extends StatelessWidget {
+  
+  static final routes = {
+    JournalEntries.routeName: (context) => JournalEntries(),
+    NewEntry.routeName: (context) => NewEntry(),
+    EntryDetails.routeName: (context) => EntryDetails()
+  };
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Journal',
+      initialRoute: JournalEntries.routeName,
+      routes: routes
+    );
+  }
 }
