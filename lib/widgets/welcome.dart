312f548fc3a155dcf79accecd5c2dcdf4a8004be
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-01 01:28:34 -0500

    Update welcome.dart

---
+++ lib/widgets/welcome.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(Icons.book_rounded),
+        Text("Journal")
+      ]
+    );
+  }
+}
