da56ce8e9c24b0fc681b165a775d3caaadb84231
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-01 01:28:31 -0500

    Update entries_list_screen.dart

--- lib/screens/entries_list_screen.dart
+++ lib/screens/entries_list_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
 import 'new_entry_screen.dart';
+import '../db/database_manager.dart';
+import '../models/journal_entry.dart';
+import '../models/journal.dart';
+import "../screens/entry_details_screen.dart";
 
 class EntriesList extends StatefulWidget {
   static const routeName = '/';
@@ -21,20 +24,47 @@ class _EntriesListState extends State<EntriesList> {
 
   void loadJournal() async {
     final databaseManager = DatabaseManager.getInstance();
-    
+    List<JournalEntry> journalEntries = await databaseManager.journalEntries();
+    setState( () {
+      journal = Journal(entries: journalEntries);
+    });
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Journal Entries'),
       ),
-      body: Center(child: Text('This is the journal entries page.')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.of(context).pushNamed(NewEntry.routeName),
-        tooltip: 'Add a journal entry.',
-        child: const Icon(Icons.add),
-      ),
+      body: buildList(context),
+      floatingActionButton: addButton(context)
     );
   }
+
+  Widget buildList(BuildContext context) {
+    return ListView.builder(itemCount: journal.entries.length, itemBuilder: (context, index) {
+      return ListTile(
+        title: Text("${journal.entries[index].title}"),
+        subtitle: Text('${journal.entries[index].dateTime}')
+      );
+    });
+  }
+
+  Widget welcome() {
+    return Column(
+      children: [
+        Icon(Icons.book_rounded),
+        Text("Journal")
+      ]
+    );
+  }
+
+  FloatingActionButton addButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () => Navigator.of(context).pushNamed(NewEntry.routeName),
+      tooltip: 'Add a journal entry.',
+      child: const Icon(Icons.add),
+    );
+  }
+
 }
