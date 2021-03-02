7634205075c1b1fb2adec66f9879c342884ed42c
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-01 19:39:57 -0500

    Update entry_details_screen.dart

--- lib/screens/entry_details_screen.dart
+++ lib/screens/entry_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/journal_entry.dart';
+import '../widgets/journal_scaffold.dart';
 
 class EntryDetailsScreen extends StatelessWidget {
   
@@ -11,24 +12,27 @@ class EntryDetailsScreen extends StatelessWidget {
     final JournalEntry entryData = ModalRoute.of(context).settings.arguments;
     final format = DateFormat('EEEE, MMMM d, y');
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(format.format(entryData.dateTime)),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '${entryData.title}',
-              style: Theme.of(context).textTheme.headline6
-            ),
-            Text('${entryData.body}'),
-            Text('Rating: ${entryData.rating}')
-          ]
-        )
+    return JournalScaffold(
+      title: format.format(entryData.dateTime),
+      body: buildDetails(context, entryData)
+    );
+  }
+
+  Widget buildDetails(BuildContext context, JournalEntry entryData) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '${entryData.title}',
+            style: Theme.of(context).textTheme.headline6
+          ),
+          Text('${entryData.body}'),
+          Text('Rating: ${entryData.rating}')
+        ]
       )
     );
   }
+
 }
