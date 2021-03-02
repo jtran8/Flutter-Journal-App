4c9570f44e43ff082e19108f4397e78d84b6a3a8
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-01 19:39:52 -0500

    Update entries_list_screen.dart

--- lib/screens/entries_list_screen.dart
+++ lib/screens/entries_list_screen.dart
@@ -4,7 +4,8 @@ import 'new_entry_screen.dart';
 import '../db/database_manager.dart';
 import '../models/journal_entry.dart';
 import '../models/journal.dart';
-import "../screens/entry_details_screen.dart";
+import '../screens/entry_details_screen.dart';
+import '../widgets/journal_scaffold.dart';
 
 class EntriesListScreen extends StatefulWidget {
   static const routeName = '/';
@@ -17,6 +18,7 @@ class _EntriesListScreenState extends State<EntriesListScreen> {
   
   Journal journal;
 
+  @override
   void initState() {
     super.initState();
     loadJournal();
@@ -32,14 +34,15 @@ class _EntriesListScreenState extends State<EntriesListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Journal Entries'),
-      ),
-      body: buildList(context),
-      floatingActionButton: addButton(context)
+    return JournalScaffold(
+      title: 'Journal Entries',
+      body: LayoutBuilder(builder: layoutDecider),
+      routeName: NewEntryScreen.routeName
     );
   }
+
+  Widget layoutDecider(BuildContext context, BoxConstraints constraints) =>
+    constraints.maxWidth < 700 ? buildList(context, verticalLayout) : buildList(context, horizontalLayout);
 
   Widget welcome() {
     return Column(
@@ -52,30 +55,51 @@ class _EntriesListScreenState extends State<EntriesListScreen> {
     );
   }
 
-
-  Widget buildList(BuildContext context) {
-    final format = DateFormat('EEEE, MMMM d, y');
+  Widget buildList(BuildContext context, layout) {
     if (journal.entries.isEmpty) {
       return welcome();
     } else {
-      return ListView.builder(itemCount: journal.entries.length, itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('${journal.entries[index].title}'),
-          subtitle: Text(format.format(journal.entries[index].dateTime)),
-          onTap: () => Navigator.of(context).pushNamed(
-            EntryDetailsScreen.routeName,
-            arguments: journal.entries[index]
-          )
-        );
-      });
+      final format = DateFormat('EEEE, MMMM d, y');
+      return layout(context, format);
     }
   }
 
-  FloatingActionButton addButton(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () => Navigator.of(context).pushNamed(NewEntryScreen.routeName, arguments: updateList),
-      tooltip: 'Add a journal entry.',
-      child: const Icon(Icons.add),
+  Widget verticalLayout(BuildContext context, DateFormat format) {
+    return ListView.builder(itemCount: journal.entries.length, itemBuilder: (context, index) {
+      return ListTile(
+        title: Text('${journal.entries[index].title}'),
+        subtitle: Text(format.format(journal.entries[index].dateTime)),
+        onTap: () => Navigator.of(context).pushNamed(
+          EntryDetailsScreen.routeName,
+          arguments: journal.entries[index]
+        )
+      );
+    });
+  }
+
+    Widget horizontalLayout(BuildContext context, DateFormat format) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(child: ListView.builder(
+          itemCount: journal.entries.length, itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('${journal.entries[index].title}'),
+              subtitle: Text(format.format(journal.entries[index].dateTime)),
+              isThreeLine: true
+            );
+          }
+        )),
+        Expanded(child: ListView.builder(
+          itemCount: journal.entries.length, itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('${journal.entries[index].title}'),
+              subtitle: Text('${journal.entries[index].body} \nRating: ${journal.entries[index].rating}'),
+              isThreeLine: true
+            );
+          }
+        ))
+      ],
     );
   }
 
