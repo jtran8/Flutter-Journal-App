56c0e784120a93dcf36c563e1dbf7d89aa20edb6
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-01 23:04:36 -0500

    Update journal_drawer.dart

--- lib/widgets/journal_drawer.dart
+++ lib/widgets/journal_drawer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class JournalDrawer extends StatefulWidget {
-  
   final setTheme;
   final state;
   JournalDrawer({Key key, this.setTheme, this.state}) : super(key: key);
