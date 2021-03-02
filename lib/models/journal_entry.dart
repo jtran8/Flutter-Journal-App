2760dcd3779d2df88307f9429cbefe6e705991d5
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-01 19:51:33 -0500

    Update journal_entry.dart

--- lib/models/journal_entry.dart
+++ lib/models/journal_entry.dart
@@ -1,12 +1,9 @@
 class JournalEntry {
+  
   final String title;
   final String body; 
   final int rating;
   final DateTime dateTime;
 
   JournalEntry({this.title, this.body, this.rating, this.dateTime});
-  
-  // String toString() {
-  //   return 'Title: $title, Body: $body, Rating: $rating, Date: $dateTime';
-  // }
 }
