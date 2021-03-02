d5bb105d9c1e4b3064c44d49a74d7c542d7afb21
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-01 23:40:12 -0500

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+// App: builds the MaterialApp and instantiates SharedPreferences
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/details_screen.dart';
