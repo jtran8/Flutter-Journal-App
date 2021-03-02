59bed8157f7e9a96b0e278d1eaebecc7ad01a016
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-01 22:57:16 -0500

    Update new_entry_screen.dart

--- lib/screens/new_entry_screen.dart
+++ lib/screens/new_entry_screen.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 import '../widgets/entry_form.dart';
 import '../widgets/journal_scaffold.dart';
 
-class NewEntryScreen extends StatelessWidget {
+class NewEntryScreen extends StatefulWidget {
   
   static const routeName = 'newEntry';
-  
+  final setTheme;
+  final state;
+
+  NewEntryScreen({Key key, this.setTheme, this.state}) : super(key: key);
+
+  @override
+  _NewEntryScreenState createState() => _NewEntryScreenState();
+}
+
+class _NewEntryScreenState extends State<NewEntryScreen> {
   @override
   Widget build(BuildContext context) {
     return JournalScaffold(
-      title: 'New Entry', 
+      title: 'New Entry',
+      setTheme: widget.setTheme,
+      state: widget.state, 
       body: EntryForm()
     );
   }
