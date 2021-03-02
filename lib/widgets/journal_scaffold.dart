9edf31ea0c9f7220f0a725b797c919b0519d1724
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-01 19:40:07 -0500

    Update journal_scaffold.dart

--- lib/widgets/journal_scaffold.dart
+++ lib/widgets/journal_scaffold.dart
@@ -1,2 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'journal_drawer.dart';
+
+class JournalScaffold extends StatelessWidget {
+  final Widget body;
+  final title;
+  final routeName;
+
+  JournalScaffold({this.body, this.title, this.routeName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      endDrawer: SettingsDrawer(),
+      body: this.body,
+      floatingActionButton: addButton(context)
+    );
+  }
+
+  FloatingActionButton addButton(BuildContext context) {
+    if (routeName != null) {
+      return FloatingActionButton(
+        onPressed: () => Navigator.of(context).pushNamed(this.routeName),
+        tooltip: 'Add a journal entry.',
+        child: const Icon(Icons.add),
+      );
+    }
+    return null;
+  }
+
+}
