507bfcb6fde92219e74442d114b0bc6b54bb1f05
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-02 00:52:50 -0500

    Update journal.dart

--- lib/models/journal.dart
+++ lib/models/journal.dart
@@ -5,4 +5,8 @@ import 'journal_entry.dart';
 class Journal {
   List<JournalEntry> entries;
   Journal({this.entries});
+
+  void addEntry(entry) {
+    entries == null ? entries = [entry] : entries.add(entry);
+  }
 }
